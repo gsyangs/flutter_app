928418a4baa05669729e5147e1819f3f75e89356
ais <894417048@qq.com>
2019-09-02 17:39:10 +0800

    随便写写

--- lib/ui/mall_store_page.dart
+++ lib/ui/mall_store_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../entity/wchat_q_model.dart';
+import '../mock/mock_data.dart';
 
 class MallStorePage extends StatefulWidget{
   @override
@@ -7,12 +9,113 @@ class MallStorePage extends StatefulWidget{
 }
 
 class MallStorePageState extends State<MallStorePage>{
+
+  FriendCircleViewModel data = friendCircleData;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
     return new Scaffold(
-      body: new Center(
-        child: new Text("这是第三个界面"),
+      body: Container(
+        margin: EdgeInsets.fromLTRB(20, 40, 20, 20),
+
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            ClipRRect(
+              borderRadius:BorderRadius.circular(6),
+              child: Image.network(
+                data.userImgUrl,
+                width: 50,
+                height: 50,
+              ),
+            ),
+            Padding(padding: EdgeInsets.only(left: 15),),
+            Flexible(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                        data.userName,
+                        style: TextStyle(
+                          color: Colors.black87,
+                          fontSize: 16,
+                        ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10),),
+                    Text(
+                      data.saying,
+                      style: TextStyle(
+                        color: Colors.black87,
+                        fontSize: 14,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(top: 10),),
+                    Image.asset("assets/images/home_banner.png"),
+                    Padding(padding: EdgeInsets.only(top: 10),),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(
+                          this.data.publishTime,
+                          style: TextStyle(
+                            fontSize: 13,
+                            color: Color(0xFF999999),
+                          ),
+                        ),
+                        Icon(
+                          Icons.comment,
+                          size: 16,
+                          color: Color(0xFF999999),
+                        ),
+                      ],
+                    ),
+                    renderComments()
+                  ],
+                )
+            )
+          ],
+        )
+      ),
+    );
+  }
+
+  Widget renderComments(){
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      padding: EdgeInsets.all(10),
+      color: Color(0xFFF3F3F5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.data.comments.map((comment){
+          return Text.rich(
+              TextSpan(
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Color(0xff333333),
+                ),
+                children: [
+                  TextSpan(
+                    text: comment.fromUser + "：",
+                    style: TextStyle(
+                      color: Color(0xFF636F80),
+                      fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  TextSpan(text: comment.content)
+                ]..insertAll(1, comment.source ? [TextSpan()]:[
+                  TextSpan(text: '回复'),
+                  TextSpan(
+                    text: comment.toUser,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF636F80),
+                    ),
+                  ),
+                ])
+              )
+          );
+        }).toList(),
       ),
     );
   }
--- lib/ui/wholesale_page.dart
+++ lib/ui/wholesale_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import '../mock/mock_data.dart';
+import '../entity/bank_card_model.dart';
 
 class WholesalePage extends StatefulWidget{
   @override
@@ -7,12 +9,115 @@ class WholesalePage extends StatefulWidget{
 }
 
 class WholesalePageState extends State<WholesalePage>{
+
+  CreditCardViewModel data = bankCardData;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return new Scaffold(
-      body: new Center(
-        child: new Text("这是第四个界面"),
+    return Container(
+      margin: EdgeInsets.fromLTRB(16,50,16,0),
+      padding: EdgeInsets.fromLTRB(20, 20, 20, 20),
+      height: 200,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+          colors: this.data.cardColors
+        ),
+        boxShadow:[
+          BoxShadow(
+            blurRadius: 6,
+            spreadRadius: 4,
+            color: Color.fromARGB(20, 0, 0, 0),
+          )
+        ],
+      ),
+      child: Stack(
+        children: <Widget>[
+          Positioned(
+            right: -100,
+            bottom: -100,
+            child: Image.asset(
+              this.data.bankLogoUrl,
+              width: 250,
+              height: 250,
+              color: Colors.white10,
+            ),
+          ),
+          Positioned(
+            left: 0,
+            top: 0,
+            right: 0,
+            bottom: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    CircleAvatar(
+                      radius: 25,
+                      backgroundColor: Colors.white,
+                      child: Image.asset(
+                        this.data.bankLogoUrl,
+                        width: 36,
+                        height: 36,
+                        fit: BoxFit.scaleDown,
+                      ),
+                    ),
+                    Padding(padding: EdgeInsets.only(left: 15),),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                            data.bankName,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold
+                            ),
+                        ),
+                        Text(
+                            data.cardType,
+                            style: TextStyle(
+                                color: Color.fromARGB(200,255,255,255),
+                                fontSize: 14,
+                                fontWeight: FontWeight.bold
+                            )
+                        )
+                      ],
+                    )
+                  ],
+                ),
+                Padding(
+                    padding: EdgeInsets.only(left: 51,top: 40),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Text(
+                            data.cardNumber,
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 19,
+                              fontWeight: FontWeight.bold
+                            ),
+                        ),
+                        Text(
+                            data.validDate,
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 19,
+                                fontWeight: FontWeight.bold
+                            ),
+                        )
+                      ],
+                    ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
